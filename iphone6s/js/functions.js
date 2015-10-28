142f47e319592c3ceec9f53428fd3280781bebee
Tung Nguyen <tungkiet@gmail.com>
2015-10-28 23:55:04 +0700

    add html iphone6s

---
+++ iphone6s/js/functions.js
@@ -0,0 +1,38 @@
+(function($){
+	$(document).ready(function (){
+        /*HEADER*/
+        $('.kmt-iconsearch-mobi').click(function(){
+            $('.kmt-search-site').toggle(300);
+        });
+
+        var newSwiper = new Swiper('.kmt-news-swiper', {
+            slidesPerView: 'auto',
+            spaceBetween: 60,
+            paginationClickable: true,
+            pagination: '.kmt-news-swiper .swiper-pagination',
+            preventClicks: false,
+            preventClicksPropagation: true
+        });
+
+        var ipswipershop = new Swiper('.ipswipershop', {
+            slidesPerView: 1,
+            spaceBetween: 5,
+            paginationClickable: true,
+            nextButton: '.ipswipershop .swiper-button-next',
+            prevButton: '.ipswipershop .swiper-button-prev',
+            preventClicks: false,
+            preventClicksPropagation: true,
+            autoplay: 3000,
+            autoplayDisableOnInteraction: false
+        });
+
+        wow = new WOW({
+            animateClass: 'animated',
+            offset:       100
+        });
+        wow.init();
+
+
+
+	});
+})(window.jQuery);
